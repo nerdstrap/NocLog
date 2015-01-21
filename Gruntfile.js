5883c1171c14bc424cd4cb7571a61351386a4d75
nerdstrap <geekstrap@live.com>
2015-01-21 14:54:01 -0500

    updated grunt file and package for jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,60 @@
-module.exports = function(grunt) {
-    'use strict';
+module.exports = function (grunt) {
+	'use strict';
 
-    // Project configuration.
-    grunt.initConfig({
-        jasmine : {
-            src: 'src/controllers/DashboardController.js',
-            options : {
-                specs : 'spec/**/*.js',
-                template: require('grunt-template-jasmine-requirejs'),
-                templateOptions: {
-                    requireConfigFile: 'src/js/main.js',
-                    requireConfig: {
-                        baseUrl: 'src/js/'
-                    }
+	// Project configuration.
+	grunt.initConfig({
+		jasmine : {
+			src : 'controllers/DashboardController.js',
+			options : {
+				specs : 'specs/**/*.js',
+				template : require('grunt-template-jasmine-requirejs'),
+				templateOptions : {
+					requireConfigFile : 'js/spec-main.js',
+					requireConfig : {
+						baseUrl : 'js/'
+					}
 				}
-            }
-        },
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'src/controllers/*.js',
-                'spec/**/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-				'-W030': true,
-            }
-        }
-    });
+			}
+		},
+		jshint : {
+			all : [
+				'Gruntfile.js',
+				'collections/*.js',
+				'controllers/*.js',
+				'enums/*.js',
+				'fakes/*.js',
+				'models/*.js',
+				'js/app/*.js',
+				'routers/*.js',
+				'views/*.js',
+				'specs/*.js'
+			],
+			options : {
+				"-W030": false
+			}
+		},
+		sass : {
+			dist : {
+				options : {
+					style : 'expanded'
+				},
+				files : [{
+						expand : true,
+						cwd : 'scss',
+						src : ['*.scss'],
+						dest : 'css/',
+						ext : '.css'
+					}
+				]
+			}
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 
-    grunt.registerTask('test', 'jasmine');
-    //grunt.registerTask('test', ['jshint', 'jasmine']);
-
-    grunt.registerTask('default', ['test']);
-
+	grunt.registerTask('validate', ['jshint']);
+	grunt.registerTask('test', ['sass', 'jasmine']);
+	grunt.registerTask('default', ['test']);
 };
